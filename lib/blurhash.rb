b4c13ff4701a6a960ceeded5abc958f2bd19e3dd
Nolan J Tait <nolanjtait@gmail.com>
2025-05-28 19:09:24 -0700

    Adds public interface for encoding

--- lib/blurhash.rb
+++ lib/blurhash.rb
@@ -7,4 +7,8 @@ require_relative "blurhash/encode"
 module Blurhash
   class Error < StandardError; end
   # Your code goes here...
+
+  def self.encode(pixels:, width:, height:, component_x: 4, component_y: 3)
+    Encode.call(pixels:, width:, height:, component_x:, component_y:)
+  end
 end
